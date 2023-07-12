38c9a3b2816813b3d100ea64d74259d5198b5294
RailTH <100100752+RailTheHedgehog@users.noreply.github.com>
2023-07-12 12:37:50 +1100

    Пока что коммит

---
+++ script.js
@@ -0,0 +1,5 @@
+let themeToggle = document.querySelector(".themeToggle_button")
+
+themeToggle.addEventListener('click', () => {
+    themeToggle.innerHTML=('<img src="icons/icons8_sun.svg" alt="" class="themeToggle_ico">')
+})
