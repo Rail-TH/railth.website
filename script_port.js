864b0943e4482967ea7e2a7138c08158f2ba9aa9
RailTH <100100752+RailTheHedgehog@users.noreply.github.com>
2023-07-13 12:54:27 +1100

    Доработал переключатель тем

--- script_port.js
+++ script_port.js
@@ -10,32 +10,13 @@ themeToggle.addEventListener("click", () => {
     if (themeToggle.innerHTML === '<img src="icons/icons8_sun.svg" alt="" class="themeToggle_ico">') {
         themeToggle.innerHTML=('<img src="icons/icons8_crescent_moon.svg" alt="" class="themeToggle_ico">');
         homePage.style.backgroundColor=("white");
-        mainName.style.color=("black");
-        tegName.style.color=("black");
-        if (portButton != undefined) {
-            portButton.style.backgroundColor=("white");
-        }
-        button.forEach(button => {
-            button.style.backgroundColor=("white");
-        })
-
-        buttonName.forEach(buttonName => {
-            buttonName.style.color=("black");
-        })
+        portButton.style.backgroundColor=("white");
+        buttonName.style.color=("black");
     }
     else {
         themeToggle.innerHTML=('<img src="icons/icons8_sun.svg" alt="" class="themeToggle_ico">');
         homePage.style.backgroundColor=("rgb(32, 32, 32)");
-        mainName.style.color=("white");
-        tegName.style.color=("white");
-        if (portButton != undefined) {
-            portButton.style.backgroundColor=("#3d3d3d");
-        }
-        button.forEach(button => {
-            button.style.backgroundColor=("#3d3d3d");
-        })
-        buttonName.forEach(buttonName => {
-            buttonName.style.color=("white");
-        })
+        portButton.style.backgroundColor=("#3d3d3d");
+        buttonName.style.color=("white");
     }
 })
